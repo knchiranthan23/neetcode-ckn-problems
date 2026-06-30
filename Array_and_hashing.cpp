ff64e60ee93a53592c4ac55a4f4cdd3ba02599f8
K N Chiranthan <chiranthangowda41@gmail.com>
2026-06-30 17:26:21 +0530

    Added Leetcode-36 Valid Sudoku

--- Array_and_hashing.cpp
+++ Array_and_hashing.cpp
@@ -140,3 +140,53 @@
 //         }
 //         return answer;
 //     }
+
+/*Leetcode-36 Valid Sudoku*/
+// bool isValidSudoku(vector<vector<char>>& board) {
+//         for(int i=0;i<9;i++)
+//         {
+//             unordered_set<char>st;
+//             for(int j=0;j<9;j++)
+//             {
+//                if(board[i][j]== '.') continue;
+//                if(st.count(board[i][j]))
+//                {
+//                  return false;
+//                }
+//                st.insert(board[i][j]);
+//             }
+//         }
+//         for(int i=0;i<9;i++)
+//         {
+//             unordered_set<char>st;
+//             for(int j=0;j<9;j++)
+//             {
+//                if(board[j][i]== '.') continue;
+//                if(st.count(board[j][i]))
+//                {
+//                  return false;
+//                }
+//                st.insert(board[j][i]);
+//             }
+//         }
+//         for(int i=0;i<9;i+=3)
+//         {
+//             for(int j=0;j<9;j+=3)
+//             {
+//                 unordered_set<char>st;
+//                 for(int k=i;k<i+3;k++)
+//                 {
+//                   for(int l=j;l<j+3;l++)
+//                   {
+//                      if(board[k][l]=='.') continue;
+//                      if(st.count(board[k][l]))
+//                       {
+//                         return false;
+//                       }
+//                       st.insert(board[k][l]);
+//                    }
+//                  }
+//              }
+//          }
+//          return true;
+//     }
