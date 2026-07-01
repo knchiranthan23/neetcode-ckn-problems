ba6095b2ff205d1ee141cfba0b8c291dd61afbfa
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-01 13:34:00 +0530

    Added 167. Two Sum II - Input Array Is Sorted

--- Two_Pointer.cpp
+++ Two_Pointer.cpp
@@ -41,3 +41,31 @@
 //         }
 //         return true;
 //     }
+
+/*167. Two Sum II - Input Array Is Sorted*/
+// vector<int> twoSum(vector<int>& numbers, int target) {
+//         int n = numbers.size();
+//         int l=0,r=n-1;
+//         vector<int>answer;
+//         while(l<=r)
+//         {
+//             int sum=numbers[l]+numbers[r];
+//             if(sum == target)
+//             {
+//                answer.push_back(l+1);
+//                answer.push_back(r+1);
+//                break;
+//             }
+//             else if(sum>target)
+//             {
+//                 sum=sum-numbers[r];
+//                 r--;
+//             }
+//             else{
+//                 sum=sum-numbers[l];
+//                 l++;
+//             }
+            
+//         }
+//         return answer;
+//     }
