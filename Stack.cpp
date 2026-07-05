992c6ed895529de6f6001731b07be088bc6bf835
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-05 12:52:02 +0530

    Added 496. Next Greater Element I Brute Force Approach

--- Stack.cpp
+++ Stack.cpp
@@ -117,3 +117,32 @@
 //         }
 //         return st.top();
 //     }
+
+/*496. Next Greater Element I*/
+//!Brute Force Approach
+// vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>answer(nums1.size(),-1);
+//         for(int i=0;i<nums1.size();i++)
+//         {
+//             for(int j=0;j<nums2.size();j++)
+//             {
+//                if(nums1[i]==nums2[j])
+//                {
+//                  for(int k=j+1;k<nums2.size();k++)
+//                  {
+//                     if(nums2[k]>nums2[j])
+//                     {
+//                         answer[i]=nums2[k];
+//                         break;
+//                     }
+//                  }
+//                }
+//                else{
+//                   continue;
+//                }
+//             }
+//         }
+//         return answer;
+//     }
+
+//todo : Optimal Approach
