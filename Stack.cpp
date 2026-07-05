46975da2506663d2799a13b2a081c03b8f116cba
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-05 13:04:29 +0530

    Added 496. Next Greater Element I Optimal Approach

--- Stack.cpp
+++ Stack.cpp
@@ -146,3 +146,30 @@
 //     }
 
 //todo : Optimal Approach
+// vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//        int n1=nums1.size();
+//        int n2=nums2.size();
+//        unordered_map<int,int>mpp;
+//        stack<int>st;
+//        for(int i=n2-1;i>=0;i--)
+//        {
+//          while(!st.empty() && st.top()<=nums2[i])
+//          {
+//             st.pop();
+//          }
+//          if(st.empty())
+//          {
+//             mpp[nums2[i]]=-1;
+//          }
+//          else{
+//             mpp[nums2[i]]=st.top();
+//          }
+//          st.push(nums2[i]);
+//        }
+//        vector<int>answer;
+//        for(int i=0;i<n1;i++)
+//        {
+//         answer.push_back(mpp[nums1[i]]);
+//        }
+//        return answer;
+//     }
