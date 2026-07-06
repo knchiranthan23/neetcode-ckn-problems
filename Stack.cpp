2c094258095b2833b648b7e240b4467b95c4ec67
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-06 11:56:08 +0530

    Added 739.Daily Temperatures Brute Force Approach and Optimal Approach

--- Stack.cpp
+++ Stack.cpp
@@ -173,3 +173,46 @@
 //        }
 //        return answer;
 //     }
+
+/*Leetcode 739. Daily Temperatures*/
+//! Brute Force Approach
+// vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         vector<int>ans(n,0);
+//         for(int i=0;i<n;i++)
+//         {
+//             int count=1;
+//             for(int j=i+1;j<n;j++){
+//                 if(temperatures[i]<temperatures[j])
+//                 {
+//                     ans[i]=count;
+//                     break;
+//                 }
+//                 count++;
+//             }
+//         }
+//         return ans;
+//     }
+
+//todo : Optimal Approach
+// vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         vector<int>ans(n);
+//         stack<int>st;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             while(!st.empty() && temperatures[st.top()]<=temperatures[i])
+//             {
+//                 st.pop();
+//             }
+//             if(st.empty())
+//             {
+//                 ans[i]=0;
+//             }
+//             else{
+//                 ans[i]=st.top()-i;
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
