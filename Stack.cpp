704d30bec9fa2aaacb638841b446f86dd80b8c7a
K N Chiranthan <chiranthangowda41@gmail.com>
2026-08-17 20:33:01 +0530

    Added LC-907 sum of subarray minimums Optimal Apporach

--- Stack.cpp
+++ Stack.cpp
@@ -453,3 +453,53 @@
 //         int mod = 1e9+7;
 //         return (long long)(sum%mod);
 //     }
+
+//? Optimal Approach
+/* vector<int>FindNSE(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int>nse(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--)
+        {
+           while(!st.empty() && arr[st.top()]>=arr[i])
+           {
+              st.pop();
+           }
+           nse[i]=st.empty()?n:st.top();
+           st.push(i);
+        }
+        return nse;
+    }
+    vector<int>FindPSEE(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int>psee(n);
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+           while(!st.empty() && arr[st.top()]>arr[i])
+           {
+              st.pop();
+           }
+           psee[i]=st.empty()?-1:st.top();
+           st.push(i);
+        }
+        return psee;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int>nse = FindNSE(arr);
+        vector<int>psee = FindPSEE(arr);
+        int sum=0;
+        int mod = 1e9+7;
+        for(int i=0;i<n;i++)
+        {
+            int left = i-psee[i];
+            int right = nse[i]-i;
+            long long freq = left*right*1LL;
+            int val = (freq*arr[i]*1LL)%mod;
+            sum = (sum+val)%mod;
+        }
+        return sum;
+    }*/
